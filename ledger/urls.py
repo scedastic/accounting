e904a21fcddf1f3d79fa3923b75ebefc93fc3f57
ysheinfil <ysheinfil@premierhomehealthcare.com>
2025-02-06 10:20:13 -0500

    transactions start

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ChartOfAccountsView.as_view(), name='accounts'),
+    path('transaction/create', views.create_transaction, name='create-transaction'),
 ]
--- ledger/views.py
+++ ledger/views.py
@@ -6,3 +6,10 @@ from .models import Account
 class ChartOfAccountsView(LoginRequiredMixin, ListView):
     model = Account
     template_name = 'ledger/account_list.html'
+
+
+def create_transaction(request):
+    context = {
+        'placeholder': 'This is just a placeholder'
+    }
+    return render(request, 'ledger/create_transaction.html', context)
